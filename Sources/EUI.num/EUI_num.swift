451664b62531e3c0ae8d629e27163c9774003b2d
Zach Eriksen <zmeriksen@icloud.com>
2021-03-05 17:22:15 -0600

    Added EUIAppView

--- Sources/EUI.num/EUI_num.swift
+++ Sources/EUI.num/EUI_num.swift
@@ -27,6 +27,31 @@ public struct EUIScreenView: View {
     }
 }
 
+public struct EUIScreen {
+    public let id: String
+    public let title: String
+    public let backgroundColor: EUIColor
+    public let headerView: EUIView?
+    public let bodyView: EUIView
+    public let footerView: EUIView?
+    
+    public init(
+        id: String,
+        title: String,
+        backgroundColor: EUIColor,
+        headerView: EUIView? = nil,
+        bodyView: EUIView,
+        footerView: EUIView? = nil
+    ) {
+        self.id = id
+        self.title = title
+        self.backgroundColor = backgroundColor
+        self.headerView = headerView
+        self.bodyView = bodyView
+        self.footerView = footerView
+    }
+}
+
 public enum EUIView {
     case label(SomeLabel)
     case button(SomeButton)
@@ -72,6 +97,19 @@ public enum EUIColor {
     }
 }
 
+public struct EUIAppView: View {
+    public let app: EUIApp
+    
+    public init(app: EUIApp) {
+        self.app = app
+    }
+    
+    
+    public var body: some View {
+        EUIScreenView(screen: app.initialScreen)
+    }
+}
+
 public class EUIApp {
     @Observed var action: String?
     
@@ -102,31 +140,6 @@ public class EUIApp {
                 return output
             }
         )
-    }
-}
-
-public struct EUIScreen {
-    public let id: String
-    public let title: String
-    public let backgroundColor: EUIColor
-    public let headerView: EUIView?
-    public let bodyView: EUIView
-    public let footerView: EUIView?
-    
-    public init(
-        id: String,
-        title: String,
-        backgroundColor: EUIColor,
-        headerView: EUIView? = nil,
-        bodyView: EUIView,
-        footerView: EUIView? = nil
-    ) {
-        self.id = id
-        self.title = title
-        self.backgroundColor = backgroundColor
-        self.headerView = headerView
-        self.bodyView = bodyView
-        self.footerView = footerView
     }
 }
 
